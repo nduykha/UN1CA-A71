7287c2ec0538ee5e0d29d0ffc69f91dd8d2700b8
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-05-13 23:38:24 +0200

    unica: patches: bt-lib-patch: update
    - Update hex patch to support V libbluetooth_jni
    - Update code to make use of the new utils
    
    Co-authored-by: 3arthur6 <arthur.trouillot@gmail.com>
    Change-Id: I16510df4787a99b7cde8b6c8acb45f5a17cd10ae

--- unica/patches/bt-lib-patch/customize.sh
+++ unica/patches/bt-lib-patch/customize.sh
@@ -1,20 +1,30 @@
 if [ ! -f "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" ]; then
-    [ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+    LOG_STEP_IN "- Extracting libbluetooth_jni.so from com.android.btservices.apex"
+
+    [ -d "$TMP_DIR" ] && EVAL "rm -rf \"$TMP_DIR\""
     mkdir -p "$TMP_DIR"
 
-    unzip -q -j "$WORK_DIR/system/system/apex/com.android.btservices.apex" \
-        "apex_payload.img" -d "$TMP_DIR"
+    EVAL "unzip -j \"$WORK_DIR/system/system/apex/com.android.btservices.apex\" \"apex_payload.img\" -d \"$TMP_DIR\""
+
+    if ! sudo -n -v &> /dev/null; then
+        LOG "\033[0;33m! Asking user for sudo password\033[0m"
+        if ! sudo -v 2> /dev/null; then
+            ABORT "Root permissions are required to unpack APEX image"
+        fi
+    fi
 
     mkdir -p "$TMP_DIR/tmp_out"
-    sudo mount -o ro "$TMP_DIR/apex_payload.img" "$TMP_DIR/tmp_out"
-    sudo cat "$TMP_DIR/tmp_out/lib64/libbluetooth_jni.so" > "$WORK_DIR/system/system/lib64/libbluetooth_jni.so"
+    EVAL "sudo mount -o ro \"$TMP_DIR/apex_payload.img\" \"$TMP_DIR/tmp_out\""
+    EVAL "sudo cat \"$TMP_DIR/tmp_out/lib64/libbluetooth_jni.so\" > \"$WORK_DIR/system/system/lib64/libbluetooth_jni.so\""
 
-    sudo umount "$TMP_DIR/tmp_out"
+    EVAL "sudo umount \"$TMP_DIR/tmp_out\""
     rm -rf "$TMP_DIR"
 
     SET_METADATA "system" "system/lib64/libbluetooth_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+    LOG_STEP_OUT
 fi
 
-# https://github.com/3arthur6/BluetoothLibraryPatcher/blob/425bb59da6505c962a38c143137698849b01d470/hexpatch.sh#L12
+# https://github.com/3arthur6/BluetoothLibraryPatcher/blob/e22da26ae7eb0856f1342bb565e6aa26a5ccaa73/hexpatch.sh#L12
 HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-    "6804003528008052" "2b00001428008052"
+    "480500352800805228" "530100142800805228"
