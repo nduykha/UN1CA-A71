4e35afc20fc8e7350eccd7357c075ab90c79183b
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-05-18 12:11:20 +0200

    unica: patches: debloat: parallelize
    Change-Id: I8e3335177e604b894e806601cf03189220b55049

--- unica/patches/debloat/customize.sh
+++ unica/patches/debloat/customize.sh
@@ -1,39 +1,41 @@
 # Dexpreopt
-while IFS= read -r f; do
-    DELETE_FROM_WORK_DIR "product" "${f//$WORK_DIR\/product\//}"
-done < <(find "$WORK_DIR/product" -type d -name "oat")
-while IFS= read -r f; do
-    DELETE_FROM_WORK_DIR "system" "${f//$WORK_DIR\/system\//}"
-done < <(find "$WORK_DIR/system" -type d -name "oat")
+find "$WORK_DIR/product" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "product" "${1//$WORK_DIR\/product\//}"' "bash" "{}"
+find "$WORK_DIR/system" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system" "${1//$WORK_DIR\/system\//}"' "bash" "{}"
 DELETE_FROM_WORK_DIR "system" "system/etc/boot-image.bprof"
 DELETE_FROM_WORK_DIR "system" "system/etc/boot-image.prof"
 DELETE_FROM_WORK_DIR "system" "system/framework/arm"
 DELETE_FROM_WORK_DIR "system" "system/framework/arm64"
-while IFS= read -r f; do
-    DELETE_FROM_WORK_DIR "system" "${f//$WORK_DIR\/system\//}"
-done < <(find "$WORK_DIR/system/system/framework" -type f -name "*.vdex")
+find "$WORK_DIR/system/system/framework" -type f -name "*.vdex" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system" "${1//$WORK_DIR\/system\//}"' "bash" "{}"
 if $TARGET_HAS_SYSTEM_EXT; then
-   while IFS= read -r f; do
-       DELETE_FROM_WORK_DIR "system_ext" "${f//$WORK_DIR\/system_ext\//}"
-   done < <(find "$WORK_DIR/system_ext" -type d -name "oat")
+    find "$WORK_DIR/system_ext" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
+        bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system_ext" "${1//$WORK_DIR\/system_ext\//}"' "bash" "{}"
 fi
 
 # ROM & device-specific debloat list
 [ -f "$SRC_DIR/unica/debloat.sh" ] && source "$SRC_DIR/unica/debloat.sh"
 [ -f "$SRC_DIR/target/$TARGET_CODENAME/debloat.sh" ] && source "$SRC_DIR/target/$TARGET_CODENAME/debloat.sh"
 
-for f in $ODM_DEBLOAT; do
-    DELETE_FROM_WORK_DIR "odm" "$f"
-done
-for f in $PRODUCT_DEBLOAT; do
-    DELETE_FROM_WORK_DIR "product" "$f"
-done
-for f in $SYSTEM_DEBLOAT; do
-    DELETE_FROM_WORK_DIR "system" "$f"
-done
-for f in $SYSTEM_EXT_DEBLOAT; do
-    DELETE_FROM_WORK_DIR "system_ext" "$f"
-done
-for f in $VENDOR_DEBLOAT; do
-    DELETE_FROM_WORK_DIR "vendor" "$f"
-done
+ODM_DEBLOAT="$(sed "/^$/d" <<< "$ODM_DEBLOAT" | sort)"
+PRODUCT_DEBLOAT="$(sed "/^$/d" <<< "$PRODUCT_DEBLOAT" | sort)"
+SYSTEM_DEBLOAT="$(sed "/^$/d" <<< "$SYSTEM_DEBLOAT" | sort)"
+SYSTEM_EXT_DEBLOAT="$(sed "/^$/d" <<< "$SYSTEM_EXT_DEBLOAT" | sort)"
+VENDOR_DEBLOAT="$(sed "/^$/d" <<< "$VENDOR_DEBLOAT" | sort)"
+
+[ "$ODM_DEBLOAT" ] && xargs -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "odm" "$1"' "bash" "{}" \
+    <<< "$ODM_DEBLOAT"
+[ "$PRODUCT_DEBLOAT" ] && xargs -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "product" "$1"' "bash" "{}" \
+    <<< "$PRODUCT_DEBLOAT"
+[ "$SYSTEM_DEBLOAT" ] && xargs -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system" "$1"' "bash" "{}" \
+    <<< "$SYSTEM_DEBLOAT"
+[ "$SYSTEM_EXT_DEBLOAT" ] && xargs -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system_ext" "$1"' "bash" "{}" \
+    <<< "$SYSTEM_EXT_DEBLOAT"
+[ "$VENDOR_DEBLOAT" ] && xargs -I "{}" -P "$(nproc)" \
+    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "vendor" "$1"' "bash" "{}" \
+    <<< "$VENDOR_DEBLOAT"
